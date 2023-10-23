c749c3b2d9557b96d12bed6c498a434b42578aeb
Patrick Geshel <pjageshel@gmail.com>
2023-10-23 21:19:33 +0200

    working version with lookmumnocomputer functional added some variables to move to millis methods to get to BPM sequences

--- test-keyboard-sequencer.ino
+++ test-keyboard-sequencer.ino
@@ -7,8 +7,24 @@ int resetPin = 18;
 int zeroPin = 13;
 
 int currentStep = 0;
-int direction = 0;
+int direction = 0; //direction sequence or step will go : 1 for forward, -1 for reverse, 0 for current step
+int directionNow = 1; //direction now chosen : 1 = forward, -1 = reverse
 int keyState = 0; // 0 = steady state (off/LOW), this is used to make sure we do not count 1 press as multiples by checking for release
+
+//define variables (for BPM and millis)
+int BPM = 120;
+int BPMnow = BPM;
+bool stepTriggered = false; //true if a step has been triggered
+bool sequenceProgramMode = false; //if true then in sequence program mode where sequence will proceed at BPM
+
+
+bool inZeroMode = false;
+//unsigned long sequenceStepTime = 0;
+unsigned long sequenceStepTimeStart = millis();
+unsigned long sequenceStepTimeNext = sequenceStepTimeStart + 60/BPM*1000; //given current sequence time, this will provide the next moment when the sequence will move in direction given
+unsigned long buttonPressTime = 0; //millis when pressed
+unsigned long buttonHoldTime = 0; // difference, eg = millis() - buttonPressTime;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,8 +39,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   // keyboard presses
   for (int i = 0; i < 8; i++) {
     if (digitalRead(buttonPins[i])== HIGH) {
@@ -32,6 +46,7 @@ void loop() {
     }
   }
 
+  // switch presses
   if (keyState == 0) {
     if (digitalRead(forwardPin) == HIGH) {
       keyState = 1;
@@ -42,20 +57,25 @@ void loop() {
       }
     } else if (digitalRead(reversePin) == HIGH) {
       keyState = 1;
-      if (currentStep == 0) {
+      if (currentStep <= 0) {
         currentStep = 7;
       } else {
         currentStep -= 1;
       }
-    } 
-  } else if (digitalRead(forwardPin) == LOW && digitalRead(reversePin) == LOW && keyState == 1){
-    keyState = 0;
     }
+    if (digitalRead(zeroPin) == HIGH) {
+      keyState = 1;
+      currentStep = 0;
+    } else if (digitalRead(resetPin) == HIGH) {
+      keyState = 1;
+      currentStep = -1;
+    }
+  } else if (keyState == 1 && digitalRead(forwardPin) == LOW && digitalRead(reversePin) == LOW && digitalRead(zeroPin) == LOW && digitalRead(resetPin) == LOW ) {
+    keyState = 0;
+  }
 
-
-
+  //ouput
   for (int i = 0; i < 8; i++) {
-    
     if (currentStep == i) {
       digitalWrite(stepPins[i], HIGH);
     } else {

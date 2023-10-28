015d6df48b569f6e020584c5f27aeb93d8b446d1
Patrick Geshel <pjageshel@gmail.com>
2023-10-28 15:06:18 +0200

    some test stuff

--- test-keyboard-sequencer.ino
+++ test-keyboard-sequencer.ino
@@ -18,7 +18,8 @@ int directionNow = 1; //direction now chosen : 1 = forward, -1 = reverse
 int swiState = 0; // 0 = steady state (off/LOW), this is used to make sure we do not count 1 press as multiples by checking for release
 int btnState[] = {0,0,0,0,0,0,0,0};
 int autoArpeggiator[8];
-int autoBtnMode = 1; //1 = excluding buttons/steps @BPM, 1 = output record mode order of presses @BPM, 2 = ouput record mode order of presses, holds and spaces
+int autoBtnMode = 0; //0 = excluding buttons/steps @BPM, 1 = output record mode order of presses @BPM, 2 = ouput record mode order of presses, holds and spaces
+int autoRecordingStep = 0; // in autoBtnMode 1 or 2 used to track which step we are at for output.
 
 //define variables (for BPM and millis)
 int BPM = 60;
@@ -149,10 +150,20 @@ void loop() {
   // ouput assimilation
   // automode
   if (autoMode && millisNow > sequenceStepTimeNext) { //if true then next step in automode has been surpassed so lets trigger a step
+    sequenceStepTimeStart = millis();
     if(autoBtnMode <= 1 ) { //modes that follow BPM
-      sequenceStepTimeStart = millis();
       sequenceStepTimeNext = sequenceStepTimeStart + (60L*1000)/BPM;//60/BPM*1000;
       stepTriggered = true;
+    } else if (autoBtnMode == 2) { //mode that follows recorded input
+      //here it will need to follow the lists provided to know when to start 
+      sequenceStepTimeStart = millis();
+      sequenceStepTimeNext = sequenceStepTimeStart + btnPressTime[autoRecordingStep]; 
+      autoRecordingStep +=1;
+      //check if autoRecordingStep is above number of steps if so then startover.
+      //!!! need to fix output, clear all ouput
+      //clear all output except output button
+      //then
+      //~~~~>?? currentStep = autoRecordingBtn[autoRecordingStep];
     }
   }
 
